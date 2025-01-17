ac657075655b5f95957d1b045169cf3b9ca37b86
benyo <benyo@DORIANOPC>
2025-01-17 14:15:39 +0100

    Add project files.

---
+++ ParkingManagementSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ParkingManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ParkingManagementSystem.ui.Menu.welcomeScreen();
+            ParkingManagementSystem.ui.Menu.showChooseOptions();
+
+        }
+    }
+}
---
+++ ParkingManagementSystem/ui/Menu.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace ParkingManagementSystem.ui
+{
+    public static class Menu
+    {
+        public static void welcomeScreen()
+        {
+            DateTime today = DateTime.Today;
+            DateTime now = DateTime.Now;
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Welcome to the Parking Management System! ");
+            Console.WriteLine($"Today is {today.ToString("dd-MM-yyyy")}");
+            Console.WriteLine($"The Time is {now.ToString("HH:mm")}");
+
+            Console.WriteLine("-------------------------------------------");
+        }
+
+        public static void showChooseOptions() {
+            Console.WriteLine("Select the wanted action: ");
+            Console.WriteLine("1. I park my car");
+            Console.WriteLine("2. I am taking out my car");
+            Console.WriteLine("3. Pay for parking");
+            Console.WriteLine("4. Extend parking time");
+            Console.WriteLine("5. Reporting a problem");
+        }
+
+
+    }
+}
